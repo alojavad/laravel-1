a39c5c914a5917516fa3891ed0c476e14e1ef9f0
alojavad <had.hamed@ymail.com>
2015-06-19 08:42:28 +0430

    update 1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
-
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<title>Hightr</title>
+    <link rel="shortcut icon" href="{!! asset('/hightr.ico') !!}" type="image/x-icon">
+	<link href="{!! asset('/css/bootstrap.css') !!}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -19,44 +19,22 @@
 	<![endif]-->
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
+<div class="container">
+    <img src="{!! asset('/images/707.png') !!}" class="" alt="Responsive image" height="150px" width="100%">
+@yield('content');
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul>
 
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
+    <footer class="col-md-12">
 
-	@yield('content')
+        <div class="content  text-center">
+            <p>Hightr is a trademark of Javad Hajiyan. Copyright © Javad Hajiyan.</p>
+            <p ><a href="http://jackmcdade.com/">Design by Javad Hajiyan</a></p>
+        </div>
+    </footer>
+</div>
 
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<!-- Scripts -->
+<script src="{!! asset('js/jquery-1.11.2.min.js') !!}" ></script>
+<script src="{!! asset('js/bootstrap.js') !!}" ></script>
 </body>
 </html>
